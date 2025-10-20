115041755344ff88feb2f50682fdfe44076cdbfa
Alejandro Samudio <alejandro@example.com>
2025-10-20 14:22:49 -0500

    Initial commit - Tiptap Simple Editor setup

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import './index.css';
+import "@/styles/_variables.scss";
+import "@/styles/_keyframe-animations.scss";
+import "@/index.css"; // if you want to keep base styles
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
